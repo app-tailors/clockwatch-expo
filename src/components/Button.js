41bdd19629966b856449d36b52e7a4dbf40b53bc
Fredius Tout Court <frediustcdev@gmail.com>
2019-02-01 16:11:31 +0000

    update btn

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Button extends Component {
+  state = {};
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default Button;
